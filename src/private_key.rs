92e2a02db4ffeb6ebe1a4ad24eab8b6dcbcb6170
guilherme <devguilhermeribeiro000@gmail.com>
2025-03-13 22:10:36 -0300

    Alter private key content description

--- src/private_key.rs
+++ src/private_key.rs
@@ -3,7 +3,9 @@ use sha2::{Digest, Sha256};
 pub fn generate() -> [u8; 32] {
     let mut hasher = Sha256::new();
 
-    hasher.update(b"Conteudo que você quer usar como base para a criação da chave privada com Sha-256");
+    hasher.update(
+        b"Conteudo que voce quer usar como base para a criacao da chave privada com Sha-256",
+    );
 
     let priv_key = hasher.finalize();
 
